4a5c6563c576497dac67b4f5f9864a230ab076b4
erik gomez <erik12fgtv@gmail.com>
2022-08-03 04:31:42 -0700

    commit tree 98c052d91d2a41cc2a601d1b0e96d1b7de1c6c65

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+const nameAndEmail = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(nameAndEmail),
+};
+
+function submitData (name, email) {
+    return fetch("http://localhost:3000/users", configObject)
+        .then(resp => resp.json())
+        .then(object => document.querySelector('body').innerHTML = object.id)
+        .catch(error => {
+            alert("something went wrong!");
+            document.querySelector('body').append(error.message);
+        })
+}
